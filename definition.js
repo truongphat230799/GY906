dd43fa9e734bc404648c05ebf7c5be68f7e834c2
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-05-23 19:25:38 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -20,7 +20,7 @@ Blockly.Python["mlx90614_read_ambient_temp"] = function(block) {
   Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
   Blockly.Python.definitions_["import_mlx90614"] = "from mlx90614 import MLX90614";
   Blockly.Python.definitions_["import_create_mlx90614"] = "mlx90614 = MLX90614(SoftI2C(scl=Pin(22), sda=Pin(21)))";
-  var code = "mlx90614.get_amb_temp()\n";
+  var code = "mlx90614.read_ambient_temp()";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
@@ -45,7 +45,7 @@ Blockly.Python["mlx90614_read_object_temp"] = function(block) {
   Blockly.Python.definitions_['import_i2c'] = 'from machine import Pin, SoftI2C';
   Blockly.Python.definitions_["import_mlx90614"] = "from mlx90614 import MLX90614";
   Blockly.Python.definitions_["import_create_mlx90614"] = "mlx90614 = MLX90614(SoftI2C(scl=Pin(22), sda=Pin(21)))";
-  var code = "mlx90614.get_obj_temp()\n";
+  var code = "mlx90614.read_object_temp()";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
